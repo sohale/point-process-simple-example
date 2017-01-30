4423b781b57c176926195ca0ba5928247c13de98
Sohail <sohale@gmail.com>
2017-01-30 18:29:53 +0000

    x_k IIR

--- simult.py
+++ simult.py
@@ -1,11 +1,12 @@
+import math
 import numpy as np
 
-def simulate_input():
+def simulate_input(K):
     """ """
 
     # Simulation Time Length (Intervals)
     # T =
-    K = 100
+    #K = 100
     #Delta = T / float(K)
 
     Delta = 100 * MSEC
@@ -24,7 +25,7 @@ def simulate_input():
         yield t,k,I_k
 
 range(2)
-n0 = {'rho': 0.99, 'alpha': 3.0, 'sigma_eps2':0.001, 'mu': -4.9, 'beta': 0.0}
+n0 = {'rho': 0.99, 'alpha': 3.0, 'sigma_eps':math.sqrt(0.001), 'mu': -4.9, 'beta': 0.0}
 BETA_RANGE = [0.9, 1.1]
 NEURONS = 20
 MSEC = 0.001
@@ -32,15 +33,21 @@ MSEC = 0.001
 na = []
 for i in range(NEURONS):
     n = n0.copy()
-    d = BETA_RANGE[1] - BETA_RANGE[0]
+    d = BETA_RANGE[1] - BETA_RANGE[0]sigma_eps2
     n['beta'] = (np.random.rand() * d) + BETA_RANGE[0]
     na.append(n)
 
-#for n in na:
+#for n in na:eps_k
 #    print n['beta']
 
 
 
-
-for t,k,I_k in simulate_input():
+K = 100
+x_arr = np.zreos((K,))
+for t,k,I_k in simulate_input(K):
     print t,k,I_k
+
+    n = na[0]
+
+    eps_k = n['sigma_eps'] * np.random.randn()
+    x_k = n['rho'] * last_x_k  + n['alpha'] * I_k + eps_k
